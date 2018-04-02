568380fc149e6db0d3fe048c0f4131472d83c7ec
Gert Cuykens <gert.cuykens@gmail.com>
2018-04-03 00:48:55 +0200

    tls

---
+++ path.go
@@ -0,0 +1,41 @@
+package tls
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+)
+
+// Path + GOPATH
+func Path(rel string) string {
+	if filepath.IsAbs(rel) {
+		return rel
+	}
+	v, err := goPackagePath("github.com/gertcuykens/tls")
+	if err != nil {
+		log.Fatalf("Error finding github.com/gertcuykens/tls directory: %s", err)
+	}
+	return filepath.Join(v, rel)
+}
+
+func goPackagePath(pkg string) (path string, err error) {
+	gp := os.Getenv("GOPATH")
+	if gp == "" {
+		return path, os.ErrNotExist
+	}
+	for _, p := range filepath.SplitList(gp) {
+		dir := filepath.Join(p, "src", filepath.FromSlash(pkg))
+		fi, err := os.Stat(dir)
+		if os.IsNotExist(err) {
+			continue
+		}
+		if err != nil {
+			return "", err
+		}
+		if !fi.IsDir() {
+			continue
+		}
+		return dir, nil
+	}
+	return path, os.ErrNotExist
+}
