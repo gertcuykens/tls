36fab5aa1f14f17742b0f7066e4c67dff2c767cb
Gert Cuykens <gert.cuykens@gmail.com>
2018-12-30 08:24:27 +0100

    server

--- tls.sh
+++ tls.sh
@@ -11,22 +11,30 @@ case ${1-0} in
 }
 EOF
   echo '{"key":{"algo":"ecdsa","size":256}}' | cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=cfssl.json \
-    -hostname="localhost,127.0.0.1" - | cfssljson -bare tls
+    -hostname="localhost,127.0.0.1,$SERVER" - | cfssljson -bare tls
   exit
   ;;
-i)  
+i)
   mkdir -p /c/etc/ssl/private
   mkdir -p /c/etc/ssl/certs
   cp ca-key.pem /c/etc/ssl/private
   cp ca.pem /c/etc/ssl/certs
   cp tls-key.pem /c/etc/ssl/private
   cp tls.pem /c/etc/ssl/certs
+
+  mkdir -p /etc/ssl/private
+  mkdir -p /etc/ssl/certs
+  cp ca-key.pem /etc/ssl/private
+  cp ca.pem /etc/ssl/certs
+  cp tls-key.pem /etc/ssl/private
+  cp tls.pem /etc/ssl/certs
+
   scp tls-key.pem root@$SERVER:/etc/ssl/private
   scp tls.pem root@$SERVER:/etc/ssl/certs
   scp ca-key.pem root@$SERVER:/etc/ssl/private
   scp ca.pem root@$SERVER:/etc/ssl/certs
-  ssh -qt root@$SERVER chown consul:consul /etc/ssl/private/tls-key.pem
-  ssh -qt root@$SERVER chown consul:consul /etc/ssl/private/ca-key.pem
+  # ssh -qt root@$SERVER chown tls:tls /etc/ssl/private/tls-key.pem
+  # ssh -qt root@$SERVER chown tls:tls /etc/ssl/private/ca-key.pem
   ssh -qt root@$SERVER chmod 755 /etc/ssl/private
   ssh -qt root@$SERVER chmod 640 /etc/ssl/private/tls-key.pem
   ssh -qt root@$SERVER chmod 640 /etc/ssl/private/ca-key.pem
